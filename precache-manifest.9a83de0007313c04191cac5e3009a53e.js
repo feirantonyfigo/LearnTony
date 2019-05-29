5af8668416ac3a0558970ac19f40220dcbc6e93c
feirantonyfigo <feirantonyfigo.hu@gmail.com>
2019-05-29 10:40:08 -0400

    Updates

---
+++ precache-manifest.9a83de0007313c04191cac5e3009a53e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "946325235c3cf0d69d50b1ffeb104131",
+    "url": "/LearnTony/index.html"
+  },
+  {
+    "revision": "f3ada3fc67fe4b893b60",
+    "url": "/LearnTony/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "254557c4058e180939cb",
+    "url": "/LearnTony/static/css/main.c348ca5f.chunk.css"
+  },
+  {
+    "revision": "f3ada3fc67fe4b893b60",
+    "url": "/LearnTony/static/js/2.a2b4b844.chunk.js"
+  },
+  {
+    "revision": "254557c4058e180939cb",
+    "url": "/LearnTony/static/js/main.63da7b13.chunk.js"
+  },
+  {
+    "revision": "6d5952cf56515bc6fbed",
+    "url": "/LearnTony/static/js/runtime~main.ad2144b6.js"
+  },
+  {
+    "revision": "b42f3d9d97e2499bb8c6c70a4308e1e8",
+    "url": "/LearnTony/static/media/Resume.b42f3d9d.png"
+  },
+  {
+    "revision": "d3accfc81931b710241d36d360f2f0e9",
+    "url": "/LearnTony/static/media/mask.d3accfc8.png"
+  },
+  {
+    "revision": "87f12e96b82debdb29d2c5aaa54be551",
+    "url": "/LearnTony/static/media/selfie.87f12e96.png"
+  }
+]);
